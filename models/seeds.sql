67453322dc17d29eb8da965f871ef0f871578df5
Jason Hill <jasonhill@Jasons-MacBook-Pro.local>
2020-01-02 10:37:34 -0600

    updating seeds.sql

--- models/seeds.sql
+++ models/seeds.sql
@@ -3,3 +3,7 @@ USE clubsoda_db;
 INSERT INTO clubs (club_name, club_description, createdAt, updatedAt) values ("Titans Fanclub", "Fans of TN Titans", CURDATE(), CURDATE());
 INSERT INTO clubs (club_name, club_description, createdAt, updatedAt) values ("Steelers Fanclub", "Fans of Pittsburgh Steelers", CURDATE(), CURDATE());
 INSERT INTO clubs (club_name, club_description, createdAt, updatedAt) values ("NGU Club", "Fans of video games", CURDATE(), CURDATE());
+
+INSERT INTO events (event, date_of, start_at, end_at,  createdAt, updatedAt) values ("Titans Tailgate", "2020-01-09", 6:00, 5:00 CURDATE(), CURDATE());
+INSERT INTO events (event, date_of, start_at, end_at,  createdAt, updatedAt) values ("Steelers Tailgate", "2020-02-22", 6:00, 5:00 CURDATE(), CURDATE());
+INSERT INTO events (event, date_of, start_at, end_at,  createdAt, updatedAt) values ("NGU bookathon", "2020-01-11", 7:00, 3:00, CURDATE(), CURDATE());
